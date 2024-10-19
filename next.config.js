8bafd2c7662129616ef1a7c306591e17714f1b62
JoeJoeflyn <thaitainguyen336@gmail.com>
2024-10-19 19:43:38 +0700

    chore: doing chore

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  staticPageGenerationTimeout: 120,
   productionBrowserSourceMaps: false,
   experimental: {
     serverSourceMaps: false,
-    modern: true,
-    optimizeCss: true,
   },
   images: {
     domains: ["drew.Tech", "prod-files-secure.s3.us-west-2.amazonaws.com"],
